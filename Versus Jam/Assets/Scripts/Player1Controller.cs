c33c3a72723c885f0b94ba80bc99ddaed97777e6
Tcrafts2018 <49214464+Tcrafts2018@users.noreply.github.com>
2019-11-15 23:55:29 -0500

    Summon1

---
+++ Versus Jam/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Miner1 Spawned");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Attacker1 Spawned");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Heavy1 Spawned");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("Special1 Spawned");
+        }
+    }
+}
---
+++ Versus Jam/Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Debug.Log("Miner2 Spawned");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Debug.Log("Attacker2 Spawned");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            Debug.Log("Heavy2 Spawned");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            Debug.Log("Special2 Spawned");
+        }
+    }
+}
