5acb2da45e75ccffdea368117cf90e82a198a5f0
Tcrafts2018 <49214464+Tcrafts2018@users.noreply.github.com>
2019-11-15 23:22:28 -0500

    UIBeginnings

---
+++ Versus Jam/Assets/Scripts/CameraFollow2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float moveSpeed;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Time.deltaTime * Vector3.right * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Time.deltaTime * Vector3.left * moveSpeed);
+        }
+    }
+}
---
+++ Versus Jam/Assets/Scripts/SplitScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera cam1;
+    public Camera cam2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
