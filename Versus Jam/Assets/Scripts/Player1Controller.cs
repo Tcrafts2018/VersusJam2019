a6ab5564c793e7e969f917eeb3f371e2632af43a
Tcrafts2018 <49214464+Tcrafts2018@users.noreply.github.com>
2019-11-16 14:56:29 -0500

    PreImport

--- Versus Jam/Assets/Scripts/Player1Controller.cs
+++ Versus Jam/Assets/Scripts/Player1Controller.cs
@@ -8,6 +8,7 @@ public class Player1Controller : MonoBehaviour
 
     public GameObject SpawnLoc;
     public GameObject Unit;
+    private bool isSpawning;
 
     void Start()
     {
@@ -17,28 +18,42 @@ public class Player1Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1) && isSpawning == false)
         {
+            isSpawning = true;
             Debug.Log("Miner1 Spawned");
             Instantiate(Unit, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+            StartCoroutine("WaitToSpawn");
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2) && isSpawning == false)
         {
+            isSpawning = true;
             Debug.Log("Attacker1 Spawned");
             Instantiate(Unit, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+            StartCoroutine("WaitToSpawn");
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.Alpha3) && isSpawning == false)
         {
+            isSpawning = true;
             Debug.Log("Heavy1 Spawned");
             Instantiate(Unit, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+            StartCoroutine("WaitToSpawn");
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Alpha4) && isSpawning == false )
         {
+            isSpawning = true;
             Debug.Log("Special1 Spawned");
             Instantiate(Unit, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+            StartCoroutine("WaitToSpawn");
         }
+    }
+
+    IEnumerator WaitToSpawn()
+    {
+        yield return new WaitForSeconds(2);
+        isSpawning = false;
     }
 }
--- Versus Jam/Assets/Scripts/Player2Controller.cs
+++ Versus Jam/Assets/Scripts/Player2Controller.cs
@@ -6,6 +6,7 @@ public class Player2Controller : MonoBehaviour
 {
     public GameObject SpawnLoc;
     public GameObject Unit;
+    private bool isSpawning = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,28 +17,42 @@ public class Player2Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad1))
+        if (Input.GetKeyDown(KeyCode.Keypad1) && isSpawning == false)
         {
+            isSpawning = true;
             Debug.Log("Miner2 Spawned");
             Instantiate(Unit, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+            StartCoroutine("WaitToSpawn");
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad2))
+        if (Input.GetKeyDown(KeyCode.Keypad2) && isSpawning == false)
         {
+            isSpawning = true;
             Debug.Log("Attacker2 Spawned");
             Instantiate(Unit, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+            StartCoroutine("WaitToSpawn");
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad3))
+        if (Input.GetKeyDown(KeyCode.Keypad3) && isSpawning == false)
         {
+            isSpawning = true;
             Debug.Log("Heavy2 Spawned");
             Instantiate(Unit, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+            StartCoroutine("WaitToSpawn");
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad4))
+        if (Input.GetKeyDown(KeyCode.Keypad4) && isSpawning == false)
         {
+            isSpawning = true;
             Debug.Log("Special2 Spawned");
             Instantiate(Unit, SpawnLoc.transform.position, SpawnLoc.transform.rotation);
+            StartCoroutine("WaitToSpawn");
         }
+    }
+
+    IEnumerator WaitToSpawn()
+    {
+        yield return new WaitForSeconds(2);
+        isSpawning = false;
     }
 }
