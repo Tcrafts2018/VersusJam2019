4dd5357126440569a9af1897cffab7b16f3b659e
Tcrafts2018 <49214464+Tcrafts2018@users.noreply.github.com>
2019-11-16 20:48:50 -0500

    HealthWorks

--- Versus Jam/Assets/Scripts/Player1Unit.cs
+++ Versus Jam/Assets/Scripts/Player1Unit.cs
@@ -29,32 +29,43 @@ public class Player1Unit : MonoBehaviour
         if (isAttacking == true && canAttack == true)
         {
             canAttack = false;
-            P1Udamage = Random.Range(15, 26);
+            P1Udamage = Random.Range(5, 11);
             P2Uh = P2Uh - P1Udamage;
+            Debug.Log(P2Uh);
             Debug.Log("P1UAttacked");
             StartCoroutine("AttackReset");
         }
-        if (P1Uhealth <= 0)
-        {
-            Destroy(gameObject);
-            enemyDetected = false;
-        }
+
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        P2Uh = other.GetComponent<Player2Unit>().P2Uhealth;
         if (other.gameObject.tag == "Player2Unit")
         {
+            P2Uh = other.GetComponent<Player2Unit>().P2Uhealth;
             enemyDetected = true;
             isAttacking = true;
-            Debug.Log("Enemy Detected");
         }
 
         if (other.gameObject.tag == "Player1Unit")
         {
             frambDetected = true;
             Debug.Log("Framb Detected");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player2Unit")
+        {
+            P1Uhealth = other.GetComponent<Player2Unit>().P1Uh;
+            if (P2Uh <= 0)
+            {
+                Destroy(other.gameObject);
+                enemyDetected = false;
+                isAttacking = false;
+                canAttack = true;
+            }
         }
     }
 
--- Versus Jam/Assets/Scripts/Player2Unit.cs
+++ Versus Jam/Assets/Scripts/Player2Unit.cs
@@ -29,32 +29,43 @@ public class Player2Unit : MonoBehaviour
         if (isAttacking == true && canAttack == true)
         {    
             canAttack = false;
-            P2Udamage = Random.Range(15, 26);
+            P2Udamage = Random.Range(5, 11);
             P1Uh = P1Uh - P2Udamage;
+            Debug.Log(P1Uh);
             Debug.Log("P2UAttacked");
             StartCoroutine("AttackReset");
         }
-        if (P2Uhealth <= 0)
-        {
-            Destroy(gameObject);
-            enemyDetected = false;
-        }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        P1Uh = other.GetComponent<Player1Unit>().P1Uhealth;
         if (other.gameObject.tag == "Player1Unit")
         {
+            P1Uh = other.GetComponent<Player1Unit>().P1Uhealth;
             enemyDetected = true;
             isAttacking = true;
-            Debug.Log("Enemy Detected");
         }
 
         if (other.gameObject.tag == "Player2Unit")
         {
             frambDetected = true;
             Debug.Log("Framb Detected");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Player1Unit")
+        {
+            P2Uhealth = other.GetComponent<Player1Unit>().P2Uh;
+            if (P1Uh <= 0)
+            {
+                Destroy(other.gameObject);
+                enemyDetected = false;
+                isAttacking = false;
+                canAttack = true;
+            }
         }
     }
 
